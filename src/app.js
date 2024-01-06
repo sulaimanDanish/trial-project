6b021343a87d99b259b36255332601032f76814f
sulaiman <sulaiman@edii.in>
2024-01-06 16:35:03 +0530

    structure set-up/ tested

--- src/app.js
+++ src/app.js
@@ -14,5 +14,11 @@ app.use(express.urlencoded({extended:true,limit:"32kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+//routes
+
+import userRouter  from "./routes/user.routes.js";
+
+//routes declaration
+app.use("/api/v1/users",userRouter );
 
 export {app};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import connectDB from "./db/db.js";
+import {app} from "./app.js";
 
 dotenv.config({
     path: './env'
@@ -7,7 +8,7 @@ dotenv.config({
 
 connectDB()
     .then(()=>{
-        app.listen(process.env.PORT || 8000,()=>{
+        app.listen(process.env.PORT,()=>{
             console.log(`server is running at : ${process.env.PORT}`)
         })
     })
