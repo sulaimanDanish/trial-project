bf016d972fcb2b2fb6a9019fefac5b75f5acb9b5
sulaiman <sulaiman@edii.in>
2024-01-07 01:37:03 +0530

    file upload minor change

--- src/utils/fileUpload.js
+++ src/utils/fileUpload.js
@@ -1,26 +1,26 @@
-import {v2 as cloudinary} from 'cloudinary';
+import { v2 as cloudinary } from 'cloudinary';
 import fs from "fs";
-          
-cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-  api_key: process.env.CLOUDINARY_API_NAME, 
-  api_secret: process.env.CLOUDINARY_API_SECRET 
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_NAME,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
-const uploadOnCloudinary = async (localFilePath)=>{
-    try{
-        if(!localFilePath){
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) {
             return null;
         }
-        const response = await cloudinary.uploader.upload(localFilePath,{
-            resource_type:"auto"
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
         });
-        console.log("file uploaded to cloudinary",response.url);
+        fs.unlinkSync(localFilePath);
         return response;
-    }catch(error){
+    } catch (error) {
         fs.unlinkSync(localFilePath);
         return null;
     }
 }
 
-export {uploadOnCloudinary};
+export { uploadOnCloudinary };
